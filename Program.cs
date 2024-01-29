181c06aa5f4f8754a407c301801a05946c9c16fb
Odin Skandinavovich <ssatanklaus@gmail.com>
2024-01-29 20:56:22 +0200

    Finish

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
+
 class Program
 {
     struct Harchi
@@ -14,7 +15,6 @@ class Program
     }
 
     static List<Harchi> elements = new List<Harchi>();
-
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -23,15 +23,16 @@ class Program
 
         while (!exit)
         {
+            Console.WriteLine();
             Console.WriteLine("Оберiть опцiю:");
             Console.WriteLine("1. Виведення даних на екран.");
             Console.WriteLine("2. Додавання даних з клавiатури.");
             Console.WriteLine("3. Додавання даних з файлу.");
             Console.WriteLine("4. Обробка даних (видалення, редагування).");
-            Console.WriteLine("5. Операцiї з даними (пiдсумок, найменьше, найбiльше).");
+            Console.WriteLine("5. Операцiї з даними (розрахунок, найменьше, найбiльше).");
             Console.WriteLine("6. Збереження даних у файл.");
-            Console.WriteLine("7. Вихiд.");
-
+            Console.WriteLine("0. Вихiд.");
+            Console.WriteLine();
             int choice;
             bool isValidInput = false;
 
@@ -70,7 +71,7 @@ class Program
                             SaveDataToFile();
                             isValidInput = true;
                             break;
-                        case 7:
+                        case 0:
                             exit = true;
                             isValidInput = true;
                             break;
@@ -89,10 +90,18 @@ class Program
 
     static void DisplayData()
     {
-        foreach (Harchi element in elements)
+        if (elements.Count == 0)
         {
-            Console.WriteLine($"Назва крупи: {element.Name}, Необхiдна кiлькiсть води: {element.RequiredWater}," +
-                $" Кiлькiсть грам на порцiю: {element.Portion}, Калорiї: {element.Calories} на 100 грам");
+            Console.WriteLine("Колекція порожня. Немає даних для виведення.");
+            return;
+        }
+
+        Console.WriteLine("Перелік даних:");
+        for (int i = 0; i < elements.Count; i++)
+        {
+            Harchi element = elements[i];
+            Console.WriteLine($"{i}. {element.Name}\tНеобхідна кількість води: x{element.RequiredWater}\t" +
+                $"Кількість грам на порцію: {element.Portion}\tКалорії: {element.Calories} на 100 грам");
         }
     }
     static void AddDataFromKeyboard()
@@ -196,10 +205,13 @@ class Program
     }
     static void ProcessData()
     {
+        DisplayData();
+        Console.WriteLine();
         Console.WriteLine("Оберiть опцiю:");
         Console.WriteLine("1. Видалення елемента.");
         Console.WriteLine("2. Редагування елемента.");
-        Console.WriteLine("3. Повернутися назад.");
+        Console.WriteLine("0 Повернутися назад.");
+        Console.WriteLine();
 
         int choice;
         bool isValidInput = false;
@@ -223,17 +235,19 @@ class Program
                         EditElement();
                         isValidInput = true;
                         break;
-                    case 3:
-                        isValidInput = true; 
+                    case 0:
+                        isValidInput = true;
                         break;
                     default:
                         Console.WriteLine("Невiрний вибiр. Спробуйте ще раз.");
+                        Console.WriteLine();
                         break;
                 }
             }
             catch (FormatException)
             {
                 Console.WriteLine("Невiрний формат вводу. Спробуйте ще раз.");
+                Console.WriteLine();
             }
         }
     }
@@ -249,6 +263,7 @@ class Program
                 Harchi elementToRemove = elements[index];
                 elements.RemoveAt(index);
                 Console.WriteLine("Елемент видалено успiшно.");
+                DisplayData();
             }
             else
             {
@@ -262,7 +277,7 @@ class Program
     }
     static void EditElement()
     {
-        Console.Write("Введiть iндекс елемента для редагування: ");
+        Console.Write("Введiть номер позиції для редагування: ");
         try
         {
             int index = int.Parse(Console.ReadLine());
@@ -270,7 +285,11 @@ class Program
             if (index >= 0 && index < elements.Count)
             {
                 Harchi elementToEdit = elements[index];
-
+                Console.WriteLine($"Обрана крупа:");
+                Console.WriteLine();
+                Console.WriteLine($"{index}.{elementToEdit.Name}\tНеобхідна кількість води: {elementToEdit.RequiredWater}\t" +
+                    $"Кількість грам на порцію: {elementToEdit.Portion}\tКалорії: {elementToEdit.Calories} на 100 грам");
+                Console.WriteLine();
                 Console.WriteLine("Оберiть поле для редагування:");
                 Console.WriteLine("1. Назва крупи.");
                 Console.WriteLine("2. Потрiбна кiлькiсть води.");
@@ -317,6 +336,7 @@ class Program
                         elements[index] = elementToEdit;
 
                         Console.WriteLine("Елемент вiдредаговано успiшно.");
+                        DisplayElement(index);
                     }
                     else
                     {
@@ -338,6 +358,14 @@ class Program
             Console.WriteLine("Невiрний формат вводу iндексу. Спробуйте ще раз.");
         }
     }
+
+    static void DisplayElement(int index)
+    {
+        Harchi element = elements[index];
+        Console.WriteLine($"Відредагований вигляд: \n{element.Name}\tНеобхідна кількість води: {element.RequiredWater}\t" +
+            $"Кількість грам на порцію: {element.Portion}\tКалорії: {element.Calories} на 100 грам");
+    }
+
     static void DataOperations()
     {
         if (elements.Count == 0)
@@ -358,10 +386,123 @@ class Program
                 minCaloriesIndex = i;
             }
         }
+        Console.WriteLine();
         Console.WriteLine($"Крупа з найбільшою кількістю калорій: {elements[maxCaloriesIndex].Name}, {elements[maxCaloriesIndex].Calories} ккал.");
         Console.WriteLine($"Крупа з найменшою кількістю калорій: {elements[minCaloriesIndex].Name}, {elements[minCaloriesIndex].Calories} ккал.");
+        Console.WriteLine();
+        Console.WriteLine("Оберiть опцiю:");
+        Console.WriteLine("1. Порційний розрахунок кількості води.");
+        Console.WriteLine("2. Розрахунок на основі наявної ваги крупи.");
+        Console.WriteLine("0. Повернутися назад.");
+        Console.WriteLine();
+
+        int choice;
+        bool isValidInput = false;
+
+        while (!isValidInput)
+        {
+            Console.Write("Введiть номер вибору: ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                choice = int.Parse(input);
+
+                switch (choice)
+                {
+                    case 1:
+                        PortionCalc();
+                        isValidInput = true;
+                        break;
+                    case 2:
+                        WeightCalc();
+                        isValidInput = true;
+                        break;
+                    case 0:
+                        isValidInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("Невiрний вибiр. Спробуйте ще раз.");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Невiрний формат вводу. Спробуйте ще раз.");
+            }
+        }
     }
-    static void SaveDataToFile()
+    static void PortionCalc()
+    {
+        if (elements.Count == 0)
+        {
+            Console.WriteLine("Колекція порожня. Немає даних для розрахунку.");
+            return;
+        }
+
+        Console.Write("Введіть кількість порцій для розрахунку: ");
+        try
+        {
+            int numberOfPortions = int.Parse(Console.ReadLine());
+
+            if (numberOfPortions <= 0)
+            {
+                Console.WriteLine("Кількість порцій повинна бути більше 0.");
+                return;
+            }
+
+            Console.WriteLine($"Потрібна кількость води для обраної кількості порцій ({numberOfPortions} шт.):");
+            Console.WriteLine();
+
+            foreach (Harchi element in elements)
+            {
+                double waterForPortion = element.Portion * element.RequiredWater * numberOfPortions;
+                double RealCalories = element.Calories / 100 * element.Portion * numberOfPortions;
+
+                Console.Write($"Порція каши: {element.Name}\t{element.Portion} гр. \t" +
+                    $"Потрібно води для {numberOfPortions} порцій: {waterForPortion} гр.");
+                Console.WriteLine($"\tКалорій у {numberOfPortions} порціях:\t{RealCalories} Ккал.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Невірний формат вводу кількості порцій. Спробуйте ще раз.");
+        }
+    }
+
+
+    static void WeightCalc()
+    {
+        if (elements.Count == 0)
+        {
+            Console.WriteLine("Колекція порожня. Немає даних для розрахунку.");
+            return;
+        }
+
+        Console.Write("Введіть кількість крупи (в грамах): ");
+        try
+        {
+            double amount = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Розрахунок кількості води та калорій:");
+            Console.WriteLine();
+
+            foreach (Harchi element in elements)
+            {
+                double waterForWeight = amount * element.RequiredWater;
+                double RealCalories = element.Calories / 100 * amount;
+
+                Console.Write($"{element.Name} \t");
+                Console.WriteLine($"Потрібна Кількість води: \t{waterForWeight} гр. \t Кількість калорій у блюді: {RealCalories} Ккал.");
+                //Console.WriteLine($"Реальна кількість калорій для всього блюда: {RealCalories} Ккал.");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Невірний формат вводу. Введіть число.");
+        }
+    }
+static void SaveDataToFile()
     {
         try
         {
